5f87ce62ec33dcd7438633c81e2e5e5213f66f0e
Vincent Heuken <me@vincentheuken.com>
2016-06-07 06:19:59 -0700

    implemented hash queries

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-$("#stream_1").hide();
-
-$("#hitbox-button").click(function() {
+function showHitbox() {
   $("#stream_1").hide();
   $("#stream_2").show();
   $("#streamup-li").removeClass("active");
   $("#hitbox-li").addClass("active");
-});
+}
 
-$("#streamup-button").click(function() {
+function showStreamup() {
   $("#stream_2").hide();
   $("#stream_1").show();
   $("#streamup-li").addClass("active");
   $("#hitbox-li").removeClass("active");
-});
+}
+
+if (window.location.hash == "#streamup") {
+  showStreamup();
+} else {
+  showHitbox();
+}
+
+$("#hitbox-button").click(showHitbox);
+
+$("#streamup-button").click(showStreamup);
 
